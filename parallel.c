7ad4f7325c8a1d8c550554991a531ffb5d2b2d5e
mirroris <u455017k@ecs.osaka-u.ac.jp>
2022-12-23 12:41:42 +0900

    dfsが可笑しいかも

--- parallel.c
+++ parallel.c
@@ -5,6 +5,7 @@
 #include<stdbool.h>
 #define ui unsigned int 
 #define POWMAX 31
+#define MOD 1024
 
 /*  global for all programs */
 int size, seed;
@@ -20,9 +21,11 @@ typedef  struct Node Node;
 /*  build:  order for array -> order for tree   */
 void build(Node *np, ui *array){
     for(int i=0; i<size;i++){
-        int x = array[i];
+        ui x = array[i];
+        printf("x = %u\n = ",x);
         for(int k=31;k>=0;k--){
             if(((1<<k)& x)!=0){
+                printf("1");
                 if(np->rp==NULL){
                     Node *child = (Node *)malloc(sizeof(Node));
                     child->rp = NULL;
@@ -33,6 +36,7 @@ void build(Node *np, ui *array){
                 np = np->rp;
             }
             else {
+                printf("0");
                 if(np->lp==NULL){
                     Node *child = (Node *)malloc(sizeof(Node));
                     child->rp = NULL;
@@ -43,29 +47,70 @@ void build(Node *np, ui *array){
                 np = np->lp;
             }
         }
+        printf("\n");
     } 
     return;
 }
 
+/*  build_parallel:  order for array -> order for tree   */
+void build_parallel(Node *np, ui *array){
+    #pragma omp parallel
+    {   
+        #pragma omp for
+        for(int i=0; i<size;i++){
+            int x = array[i];
+            for(int k=31;k>=0;k--){
+                if(((1<<k)& x)!=0){
+                    if(np->rp==NULL){
+                        Node *child = (Node *)malloc(sizeof(Node));
+                        child->rp = NULL;
+                        child->lp = NULL;
+                        np->rp = child;
+                    }
+                    (np->num)++;
+                    np = np->rp;
+                }
+                else {
+                    if(np->lp==NULL){
+                        Node *child = (Node *)malloc(sizeof(Node));
+                        child->rp = NULL;
+                        child->lp = NULL;
+                        np->lp = child;
+                    }
+                    (np->num)++;
+                    np = np->lp;
+                }
+            }
+        } 
+        
+    }
+    return;
+}
+
 /* global variable for dfs  */
-ui index = 0;
+ui index;
 /*  dfs:    order for tree -> order forarray   */
 void dfs(Node *n, ui *array, int k, ui num){
     ui dfsnum=0;   
     bool leafflag = true;
+    printf("=================================\n===   dfs(%u, %u)   ===\n", 1 <<(POWMAX -k), num);
     if((n->lp)!=NULL) {
+        printf("lp->\n");
         dfs(n->lp, array, k+1, num);
         leafflag = false;
     }
     if((n->rp)!=NULL){
         dfsnum = num + (1<<(POWMAX-k));
+        printf("rp->\n");
         dfs(n->rp, array, k+1, dfsnum);
         leafflag = false;
     }
     if(leafflag){
         array[index] = num;
-        index++; 
+        printf ("array[%u] = %u\n", index, num);
+        index++;
     }
+    printf("==================================\n");
     return;
 } 
 
@@ -85,16 +130,16 @@ double sort1(ui *array){
 
     time = omp_get_wtime();
     //逐次ソート開始
-    /* compare  and  decide max, min in each set*/
     Node *root = (Node *)malloc(sizeof(Node));
     root->num = size;
     root->lp = NULL;
     root->rp = NULL;
 
     build(root, array);
+    printf("=========================\n\t === dfs start ===\t\n===========================\n");
     dfs(root, array, 0, 0);
-    //逐次ソート終了
     delete(root);
+    //逐次ソート終了
     time = omp_get_wtime() - time;
 
     int flag = 0;
@@ -117,10 +162,14 @@ double sort2(int *array){
 
     time = omp_get_wtime();
     //並列ソート開始
-    #pragma omp parallel
-    {
-        //ボゴソートの並列化は諦めました
-    }
+    Node *root = (Node *)malloc(sizeof(Node));
+    root->num = size;
+    root->lp = NULL;
+    root->rp = NULL;
+
+    build_parallel(root, array);
+    dfs(root, array, 0, 0);
+    delete(root);
     //並列ソート終了
     int flag = 0;
     for(i=0; i<size-1; i++){
@@ -152,26 +201,16 @@ void main(int argc, char *argv[])//プログラム名 大きさ シード値
 
     int i;
     for(i=0; i<size; i++){
-        array1[i] = rand();
+        array1[i] = rand()%MOD;
         array2[i] = array1[i];
     }
 
     double time1 = sort1(array1);
-    double time2;
-    //sort2(array2);
+    double time2 = sort2(array2);
 
     printf("time1 = %lf, time2 = %lf\n", time1, time2);
     printf("speedup = %lf\n", time1 / time2);
 
     free(array1);
     free(array2);
-}
-
-void shuffle(ui *array){
-    for(int i=0; i<size; i++){
-        int j = rand()%size;
-        int keep = array[i];
-        array[i] = array[j];
-        array[j] = keep;
-    }
 }
