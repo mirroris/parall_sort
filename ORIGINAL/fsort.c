f0a9ac45ca7a9d4cf095e83a5e7af7dae8c53cb5
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-01-25 00:16:17 +0900

    dir clean

---
+++ ORIGINAL/fsort.c
@@ -0,0 +1,6 @@
+#ifndef FSORT_H
+#define FSORT_H
+#include "fsort.h"
+
+void *build(ui array,)
+#endif
---
+++ PARADIS/zemib.h
@@ -0,0 +1,5 @@
+#define MOD 16
+typedef unsigned int  ui;
+ui findMax(ui *array);
+void msdRadixSort(ui *array, ui mod);
+void radixSort(ui *array, ui l, ui left, ui right);
